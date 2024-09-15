539e6b3fad633c5820ad25270718353668670ece
dominicemilio <emilio.clemence@outlook.com>
2024-09-15 14:31:15 -0500

    Uploaded program with changes

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Ask the user for their name and surname
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+
+        Console.Write("What is your surname? ");
+        string surname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {surname}, {name} {surname}!");
     }
 }
