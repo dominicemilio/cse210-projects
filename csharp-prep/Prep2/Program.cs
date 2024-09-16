ac3ca5c370541a2b96bf98e44e38cf0285a37946
dominicemilio <emilio.clemence@outlook.com>
2024-09-16 10:18:32 -0500

    Completed Prep 2 Core

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -2,8 +2,38 @@ using System;
 
 class Program
 {
+    // Main is the starting point
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        // Ask user for their grade
+        Console.Write("Please enter your grade as a percentage (0-100): ");
+        
+        // Read the input 
+        int grade = int.Parse(Console.ReadLine());
+
+        // Check the correct grade and print it
+        if (grade >= 90)
+        {
+            Console.WriteLine("Your grade is an A");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("Your grade is a B");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("Your grade is a C");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("Your grade is a D");
+        }
+        else
+        {
+            Console.WriteLine("Your grade is an F");
+        }
+        
+
     }
+
 }
