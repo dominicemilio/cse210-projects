6d05d859a8c3501a32c070beff73b2b749633bc7
dominicemilio <emilio.clemence@outlook.com>
2024-09-22 12:41:13 -0500

    upload prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // This makes a random number
+        Random random = new Random();
+
+        // Generate a random number between 1 and 100
+        byte magicNumber = (byte)random.Next(1, 101);
+
+        // Initialize user guess as integer
+        byte guess;
+
+        // Continue until the right number is guessed
+        do
+        {
+            // Get the guess
+            Console.Write("What's your guess? ");
+            guess = byte.Parse(Console.ReadLine());
+
+            // This checks if guess is low
+            if (guess < magicNumber)
+                Console.WriteLine("Higher");
+
+            // This checks if guess is high
+            else if (guess > magicNumber)
+                Console.WriteLine("Lower");
+
+            // If correct congratulate them
+            else
+                Console.WriteLine("You've got the number!");
+        } while (guess != magicNumber);
     }
 }
