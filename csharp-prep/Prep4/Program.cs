182c562bc7f2d84ab890c8f8ce7866d8f512dfe5
dominicemilio <emilio.clemence@outlook.com>
2024-09-22 15:32:54 -0500

    upload prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        int userNumber;
+
+        // I'll use a do-while loop
+        do
+        {
+            Console.Write("Enter a whole number or 0 to quit): ");
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+
+            // Add any number that is not 0 to the list
+            if (userNumber != 0)
+            {
+                numbers.Add(userNumber);
+            }
+        }
+        while (userNumber != 0);
+
+        // Add them all up
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+
+        // Get the average
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        // Determine the largest number
+        int largest = numbers[0];
+        foreach (int number in numbers)
+        {
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {largest}");
     }
 }
